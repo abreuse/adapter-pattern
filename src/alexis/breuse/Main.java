67db5a05c1d6b2b2308cc946b0da9e60cffb8a88
Alexis <alexis.breuse@gmail.com>
2019-01-02 19:06:02 +0100

    Explicit code which show the new interface being adapted to the old one

--- src/alexis/breuse/Main.java
+++ src/alexis/breuse/Main.java
@@ -4,6 +4,7 @@ import alexis.breuse.Adaptee.FastCalculator;
 import alexis.breuse.Adaptee.NewCalculator;
 import alexis.breuse.Adapter.OldNewCalculatorAdapter;
 import alexis.breuse.Client.AdditionCalculator;
+import alexis.breuse.TargetInterface.OldCalculator;
 import alexis.breuse.TargetInterface.SlowCalculator;
 
 public class Main {
@@ -19,5 +20,10 @@ public class Main {
 
         AdditionCalculator client = new AdditionCalculator(adapter);
         client.calculate(1, 3);
+
+        //inline : la nouvelle interface est adaptée pour être compatible avec l'ancienne
+        //grâce au polymorphisme des interfaces
+        OldCalculator calculator = new OldNewCalculatorAdapter(new FastCalculator());
+        calculator.add(1,4);
     }
 }
